bc777e91ba4aebba233e2576277df3a2a41104c8
Eduardo Decarli <decarliedu@gmail.com>
2025-02-17 11:59:03 -0300

    feat: creation a log for ClientSetup and CommonSetup

--- src/main/java/com/decarli/firstmod/FirstMod.java
+++ src/main/java/com/decarli/firstmod/FirstMod.java
@@ -2,14 +2,44 @@ package com.decarli.firstmod;
 
 import com.example.examplemod.ExampleMod;
 import com.mojang.logging.LogUtils;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
+
+import static net.minecraftforge.versions.forge.ForgeVersion.MOD_ID;
 
 @Mod(FirstMod.MODID2)
 public class FirstMod {
     public static final String MODID2 = "firstmod";
     private static final Logger LOGGER = LogUtils.getLogger();
     public FirstMod() {
-        LOGGER.info("Meu primeiro Mod Carregado");
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus(); // Obtém o Event Bus do mod
+
+        // Registra o método de configuração comum no Event Bus
+        modEventBus.addListener(this::commonSetup);
+
+        // Registra o método de configuração no Event Bus do cliente
+        modEventBus.addListener(this::clientSetup);
+    }
+
+    // Método que será chamado no evento FMLCommonSetupEvent
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("Configuração comum para o mod '{}'", MOD_ID);
+    }
+
+    // Método que será chamado no evento FMLClientSetupEvent
+    private void clientSetup(final FMLClientSetupEvent event) {
+        LOGGER.info("Configuração do cliente para o mod '{}'", MOD_ID);
+    }
+
+    // Este método irá ser chamado para inicializar o mod
+    @SubscribeEvent
+    public static void onModInitialization(FMLCommonSetupEvent event) {
+        LOGGER.info("Inicializando o mod '{}'", MOD_ID);
     }
 }
