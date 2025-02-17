8a6bb8b40036434ec4a317cf305fc41bcd6cc96b
Eduardo Decarli <decarliedu@gmail.com>
2025-02-17 12:21:21 -0300

    feat: event break block in log

--- src/main/java/com/decarli/firstmod/FirstMod.java
+++ src/main/java/com/decarli/firstmod/FirstMod.java
@@ -1,45 +1,32 @@
 package com.decarli.firstmod;
 
-import com.example.examplemod.ExampleMod;
 import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
-import static net.minecraftforge.versions.forge.ForgeVersion.MOD_ID;
 
 @Mod(FirstMod.MODID2)
 public class FirstMod {
     public static final String MODID2 = "firstmod";
     private static final Logger LOGGER = LogUtils.getLogger();
+
     public FirstMod() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus(); // Obtém o Event Bus do mod
-
-        // Registra o método de configuração comum no Event Bus
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::commonSetup);
-
-        // Registra o método de configuração no Event Bus do cliente
-        modEventBus.addListener(this::clientSetup);
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
-    // Método que será chamado no evento FMLCommonSetupEvent
     private void commonSetup(final FMLCommonSetupEvent event) {
-        LOGGER.info("Configuração comum para o mod '{}'", MOD_ID);
+        LOGGER.info("Configuração comum para o mod '{}'", MODID2);
     }
 
-    // Método que será chamado no evento FMLClientSetupEvent
-    private void clientSetup(final FMLClientSetupEvent event) {
-        LOGGER.info("Configuração do cliente para o mod '{}'", MOD_ID);
-    }
-
-    // Este método irá ser chamado para inicializar o mod
     @SubscribeEvent
-    public static void onModInitialization(FMLCommonSetupEvent event) {
-        LOGGER.info("Inicializando o mod '{}'", MOD_ID);
+    public void onBlockBrean(BlockEvent.BreakEvent event) {
+        LOGGER.info("O jogador {} quebrou o bloco em {}", event.getPlayer().getName(), event.getPos());
     }
 }
