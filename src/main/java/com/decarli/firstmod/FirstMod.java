df2283e500b976296f3974396da14a48b00c0669
Eduardo Decarli <decarliedu@gmail.com>
2025-02-17 23:42:40 -0300

    feat: add alexandrite

--- src/main/java/com/decarli/firstmod/FirstMod.java
+++ src/main/java/com/decarli/firstmod/FirstMod.java
@@ -1,12 +1,20 @@
 package com.decarli.firstmod;
 
+import com.decarli.firstmod.item.ModItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
@@ -16,18 +24,38 @@ public class FirstMod {
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public FirstMod() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        modEventBus.addListener(this::commonSetup);
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        bus.addListener(this::commonSetup);
+
         MinecraftForge.EVENT_BUS.register(this);
-        ModItems.register(FMLJavaModLoadingContext.get().getModEventBus());
+
+        ModItems.register(bus);
+
+        bus.addListener(this::addCreative);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
-        LOGGER.info("Configuração comum para o mod '{}'", MOD_ID);
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+        }
+    }
+
+    private void commonSetup(Event event) {
     }
 
     @SubscribeEvent
-    public void onBlockBrean(BlockEvent.BreakEvent event) {
-        LOGGER.info("O jogador {} quebrou o bloco em {}", event.getPlayer().getName(), event.getPos());
+    public void onServerStarting(ServerStartingEvent event) {
     }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+
+
+
 }
