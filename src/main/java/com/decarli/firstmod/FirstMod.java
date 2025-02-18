5fc08c73fbed2bfed55b9b23976b7f74102cbe2e
Eduardo Decarli <decarliedu@gmail.com>
2025-02-18 14:03:29 -0300

    feat: create Alexandrite mod

--- src/main/java/com/decarli/firstmod/FirstMod.java
+++ src/main/java/com/decarli/firstmod/FirstMod.java
@@ -2,19 +2,18 @@ package com.decarli.firstmod;
 
 import com.decarli.firstmod.item.ModItems;
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
@@ -39,10 +38,11 @@ public class FirstMod {
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
         }
     }
 
-    private void commonSetup(Event event) {
+    private void commonSetup(final FMLCommonSetupEvent event) {
     }
 
     @SubscribeEvent
